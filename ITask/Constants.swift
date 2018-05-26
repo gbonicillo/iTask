530bdc7a16f21a1c85b4a39f0d298cd5978bc82c
gbonicillo <38454174+gbonicillo@users.noreply.github.com>
2018-05-27 04:30:39 +1000

    Update Constants.swift

--- ITask/Constants.swift
+++ ITask/Constants.swift
@@ -5,6 +5,12 @@ import Foundation
 
 struct Constants {
     static let googleClientID: String = "170665834985-g1tupdb9t60oon3r8dmt19sf8n76hi26.apps.googleusercontent.com"
+    
+    //working
     static let twitterConsumerKey: String = "1lkN9XOHRKK6WmJ3qcvS7T3BQ"
     static let twitterConsumerSecret: String = "gvCWBYyO35r37xjYCAojRfHxiVfslOU44UQufXQGoMPPADBiPF"
+    
+    //new
+    //static let twitterConsumerKey: String = "6ZlxPTqlXAOfEEzcIWWCDeu3e"
+    //static let twitterConsumerSecret: String = "7sqBJ1pYsfDxWNtTMxOQAzkJTNuCapiMhbThUV5wNRIkrxhlWf"
 }
